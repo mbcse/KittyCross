287c879ff25c7b101c0b7bd3509e59c53a33c4b0
phillip-kemper <phillip-kemper@gmx.net>
2023-11-17 22:38:37 +0300

    add mock consts

--- frontend/kitty-cross-frontend/src/utils/constants.ts
+++ frontend/kitty-cross-frontend/src/utils/constants.ts
@@ -1,2 +1,8 @@
 
-// wip
+const contractAddresses = {
+    // just placeholders for now
+    'polygonZkEVM' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
+    'scroll' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
+    'arbitrum' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
+    'base' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
+}
