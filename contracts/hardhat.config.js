1081b527e3ed4062f20a88874660a980f7a114db
mbneobrix <mbcse50@gmail.com>
2023-11-18 01:53:51 +0300

    Fix

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -47,7 +47,7 @@ module.exports = {
   solidity: {
     compilers:[
     {
-        version: '0.8.20',
+        version: '0.8.5',
         settings: {
           optimizer: {
             enabled: true,
