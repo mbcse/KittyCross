056a15db31f086a479698514bcd79239fff02b2e
phillip-kemper <phillip-kemper@gmx.net>
2023-11-17 23:02:33 +0300

    change get nfts per address method

--- frontend/kitty-cross-frontend/src/utils/getAllNFTOfAddress.ts
+++ frontend/kitty-cross-frontend/src/utils/getAllNFTOfAddress.ts
@@ -1,31 +1,28 @@
 import { contractABI, contractAddresses } from "./constants";
-import { useContractRead } from 'wagmi';
-
-
+import { useContractRead } from "wagmi";
 
 async function getAllTokensOfOwner(ownerAddress) {
-    const { data, isError, isLoading } = useContractRead({
-        address: contractAddresses,
-        abi: contractABI,
-        functionName: 'totalSupply',
-      })
+  const { data, isError, isLoading } = useContractRead({
+    address: contractAddresses,
+    abi: contractABI,
+    functionName: "totalSupply",
+  });
 
-    const totalSupply = await contract.methods.totalSupply().call();
-    const tokens = [];
-  
-    for (let tokenId = 0; tokenId < totalSupply; tokenId++) {
-      try {
-        const tokenOwner = await contract.methods.ownerOf(tokenId).call();
-        if (tokenOwner.toLowerCase() === ownerAddress.toLowerCase()) {
-          tokens.push(tokenId);
-        }
-      } catch (error) {
-        // Handle errors (e.g., token does not exist)
+  const totalSupply = await contract.methods.totalSupply().call();
+  const tokens = [];
+
+  for (let tokenId = 0; tokenId < totalSupply; tokenId++) {
+    try {
+      const tokenOwner = await contract.methods.ownerOf(tokenId).call();
+      if (tokenOwner.toLowerCase() === ownerAddress.toLowerCase()) {
+        tokens.push(tokenId);
       }
+    } catch (error) {
+      // Handle errors (e.g., token does not exist)
     }
-  
-    return tokens;
   }
-  
-  getAllTokensOfOwner('0xOwnerAddress').then(console.log);
-  
+
+  return tokens;
+}
+
+getAllTokensOfOwner("0xOwnerAddress").then(console.log);
