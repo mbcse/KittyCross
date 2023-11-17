263d89c933c5be078800052ffa48df1d1f703015
phillip-kemper <phillip-kemper@gmx.net>
2023-11-17 22:52:38 +0300

    wip get nfts of address

--- frontend/kitty-cross-frontend/src/utils/constants.ts
+++ frontend/kitty-cross-frontend/src/utils/constants.ts
@@ -1,8 +1,10 @@
 
-const contractAddresses = {
+export const contractAddresses = {
     // just placeholders for now
     'polygonZkEVM' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
     'scroll' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
     'arbitrum' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
     'base' : '0x4458Bd6E2A8aBd2Ee3EeEaA5E1AeAeA0098f9Cf7',
 }
+
+export const contractABI = [/* ...ERC-721 ABI... */];
