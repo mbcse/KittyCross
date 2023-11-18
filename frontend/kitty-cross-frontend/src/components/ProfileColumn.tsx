0d1d5800f54ae8fe1a53f7ad154cc69e6f87038a
phillip-kemper <phillip-kemper@gmx.net>
2023-11-18 19:26:09 +0300

    adapt chains and hook

--- frontend/kitty-cross-frontend/src/components/ProfileColumn.tsx
+++ frontend/kitty-cross-frontend/src/components/ProfileColumn.tsx
@@ -4,25 +4,41 @@ import { getChainIdForNetworkName } from "../utils/constants";
 import { useGetAllKittiesData } from "../utils/useGetAllKittiesData";
 import KittyCard from "./Profile/KittyCard";
 
-export default function ProfileGrid({ chainName, networkName, imageSource, kitties }) {
-  const chainId = getChainIdForNetworkName(networkName)
-  const kittiesData = useGetAllKittiesData(chainId,kitties)
+export default function ProfileGrid({
+  chainName,
+  networkName,
+  imageSource,
+  kitties,
+}) {
+  const chainId = getChainIdForNetworkName(networkName);
+  const kittiesData = useGetAllKittiesData(chainId, kitties);
+
+
 
   return (
     <>
       <GridItem w="100%">
         <Flex justifyContent="center" alignItems="center">
-          <Image src={imageSource} alt="Logo" boxSize="40px" borderRadius='50%'/>
+          <Image
+            src={imageSource}
+            alt="Logo"
+            boxSize="40px"
+            borderRadius="50%"
+          />
         </Flex>
         <Flex justifyContent="center" alignItems="center">
           <Text fontSize="2xl">{chainName}</Text>
         </Flex>
 
-        {kitties && kitties.map((kitty) => (
+        <Flex justifyContent="center" alignItems="center">
+          {kitties}
+        </Flex>
+
+        {/* {kitties && kitties.map((kitty) => (
           <Flex key={kitty} justifyContent="center" alignItems="center">
-            {/* <KittyCard kittyId={kitty} /> */}
+            {JSON.stringify(kitties)}
           </Flex>
-        ))}
+        ))} */}
       </GridItem>
     </>
   );
