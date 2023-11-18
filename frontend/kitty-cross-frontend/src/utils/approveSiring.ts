c3ece2038993f9fdda574b8d7fd018a2ebe6c0a2
phillip-kemper <phillip-kemper@gmx.net>
2023-11-18 03:17:14 +0300

    approve siring potential flow

---
+++ frontend/kitty-cross-frontend/src/utils/approveSiring.ts
@@ -0,0 +1,32 @@
+import { useContractWrite, usePrepareContractWrite } from "wagmi";
+import { contractABI } from "./constants";
+
+async function writeApproveSiring(
+  chainIdOrigin,
+  chainIdDestination,
+  sireId,
+  matronId
+) {
+  const { config } = usePrepareContractWrite({
+    // TODO: adapt addr
+    address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
+    // TODO: adapt abi
+    abi: contractABI,
+    functionName: "approveSiring",
+    // TODO: adapt args
+    args: [sireId, chainIdDestination, matronId],
+    chainId: chainIdOrigin,
+  });
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
+
+  // Check if the request was successful
+  if (!isSuccess) {
+    throw new Error(`Error approving siring`);
+  }
+
+  return data;
+  if (!isSuccess) {
+    throw new Error(`error approving siring`);
+  }
+  return data;
+}
