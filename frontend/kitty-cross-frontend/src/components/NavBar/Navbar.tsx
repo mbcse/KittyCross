14eae5ccc809db8e8abf7f9d61bd5c879f3dc680
phillip-kemper <phillip-kemper@gmx.net>
2023-11-18 03:36:11 +0300

    fix typo

--- frontend/kitty-cross-frontend/src/components/NavBar/Navbar.tsx
+++ frontend/kitty-cross-frontend/src/components/NavBar/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar(){
                   variant="ghost"
                   leftIcon={<BsFillCameraVideoFill />}
                 >
-                  Catalouge
+                  Catalogue
                 </Button>
               </VStack>
             </Box>
