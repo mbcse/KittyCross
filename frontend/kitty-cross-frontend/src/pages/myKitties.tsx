95944813d6d539ace945f17f3e36d31eeccde3d7
phillip-kemper <phillip-kemper@gmx.net>
2023-11-18 19:50:54 +0300

    adapt owner token hook

--- frontend/kitty-cross-frontend/src/pages/myKitties.tsx
+++ frontend/kitty-cross-frontend/src/pages/myKitties.tsx
@@ -5,21 +5,27 @@ import Navbar from "../components/NavBar/Navbar";
 import Layout from "../components/Layout";
 import { useAllTokensOfOwner } from "../utils/useAllTokensOfOwner";
 import { useAccount } from "wagmi";
+import { useEffect } from "react";
 export default function MyKitties() {
-  const { address, isConnected } = useAccount()
+  const { address, isConnected } = useAccount();
   // const data = useAllTokensOfOwner(address);
   const data = {
-    "polygon-zkevm" : [1,2,3],
-    "scroll" : [4,5,6],
-    "arbitrum" : [1,2,3],
-    "base" : [4,5,6],
-  }
+    "polygon-zkevm": [1, 2, 3],
+    scroll: [4, 5, 6],
+    arbitrum: [1, 2, 3],
+    base: [4, 5, 6],
+  };
+
+  const userTokens = useAllTokensOfOwner(address);
+  useEffect(() => {
+    console.log(userTokens);
+  }, [userTokens]);
   return (
     <>
       <Navbar />
       <Layout>
         <Text fontSize="2xl">My Cats:</Text>
-        <ProfileGrid kittyData={data}/>
+        <ProfileGrid kittyData={data} />
       </Layout>
     </>
   );
